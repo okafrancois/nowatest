83ebeb2d09493199c459e6c37cbbcf8ed7ec56d1
François <itoutouberny@gmail.com>
2021-12-03 19:35:07 +0100

    Updates

---
+++ precache-manifest.4aad5de1b4fe599a9e1acaeb6d2eda34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec6a566bf1002acfb3afce18d00bef7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4b74b7a71e980a091122",
+    "url": "./static/css/main.5e6c5e86.chunk.css"
+  },
+  {
+    "revision": "9dd694d6a537bf928419",
+    "url": "./static/js/2.af67700a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.af67700a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b74b7a71e980a091122",
+    "url": "./static/js/main.0c17edac.chunk.js"
+  },
+  {
+    "revision": "5f3fea67c294c3011b3f",
+    "url": "./static/js/runtime-main.d579b51d.js"
+  }
+]);
